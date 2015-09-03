351feac876436010252250e4f794263dc30e0ddf
Robert <mam09cqz@studserv.uni-leipzig.de>
2015-09-03 14:50:44 +0200

    Reading of all data and rough print function.

--- csv2rdf.py
+++ csv2rdf.py
@@ -17,15 +17,34 @@ class Csv2Rdf:
     def readTextInput(self):
         text = self.readInputFile()
         labels = []
+        data = []
         for label in text[0].split(';', -2):
             if not '\n' in label:
-                labels.append(label)
+                if '\ufeff' in label:
+                    labels.append(label[1:])
+                else:
+                    labels.append(label)
+        data.append(labels)
+        for entry in text[1:]:
+            lineData = []
+            for value in entry.split(';', -2):
+                if not '\n' in value:
+                    lineData.append(value)
+            data.append(lineData)
+        return data
 
+    def printData(self, data):
+        output = "|"
+        for line in data:
+            for value in line:
+                output = output + value + '\t|'
+            output = output + '\n|'
+        print(output)
 
 
 def main():
     cr = Csv2Rdf(input('Name of .csv file:'))
-    cr.readTextInput()
+    cr.printData(cr.readTextInput())
 
 if __name__ == '__main__':
     main()
