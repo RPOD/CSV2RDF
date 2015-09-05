d1602a919b1093b0cf73fcc9fb9fca3fabc08e28
Robert R <mam09cqz@studserv.uni-leipzig.de>
2015-09-05 18:27:18 +0200

    Added template handling and first proper Data Cube template.

--- csv2rdf.py
+++ csv2rdf.py
@@ -7,11 +7,19 @@ class Csv2Rdf:
         self.filename = filename
 
     def readInputFile(self):
-        f = open(self.filename, 'r', encoding="utf-8")
+        f = open(self.filename + '.csv', 'r', encoding="utf-8")
         text = []
         for line in f:
             text.append(line)
         f.close()
+        return text
+
+    def readTemplate(self, name):
+        t = open(name, 'r', encoding="utf-8")
+        text = []
+        for line in t:
+            text.append(line)
+        t.close()
         return text
 
     def readTextInput(self):
@@ -33,6 +41,31 @@ class Csv2Rdf:
             data.append(lineData)
         return data
 
+    def createOutput(self, data):
+        template = self.readTemplate(self.filename + '_template.ttl')
+        if self.filename == 'employment':
+            for i, entry in enumerate(data[1:]):
+                output = ('statbel:o' + str(i + 1) + ' a qb:Observation;\n\t' +
+                        'qb:dataSet\tstatbel:dataset-employmentUnemployment;\n\t' +
+                        'sdmx-dimension:refPeriod\t"' + entry[0] + '"^^dc:date;\n\t' +
+                        'statbel:employed\t' + entry[3] + ';\n\t' +
+                        'statbel:unemployed\t' + entry[1] + ';\n\t' +
+                        'statbel:inactive\t' + entry[2] + ';\n\t.\n\n')
+                template.append(output)
+        elif self.filename == 'hpi':
+            for i, entry in enumerate(data[1:]):
+                output = ('statbel:o' + str(i + 1) + ' a qb:Observation;\n\t' +
+                        'sdmx-dimension:refPeriod\t"' + entry[0][4:6] + '/' + entry[0][:4] + '"^^dc:date;\n\t' +
+                        'statbel:inflation\t' + entry[1] + '\n\t.\n\n')
+                template.append(output)
+        of = open(self.filename + '_output.ttl', 'w', encoding="utf-8")
+        for line in template:
+            of.write(line)
+        of.close
+
+    """
+    Testmethod
+    """
     def printData(self, data):
         output = "|"
         for line in data:
@@ -44,7 +77,7 @@ class Csv2Rdf:
 
 def main():
     cr = Csv2Rdf(input('Name of .csv file:'))
-    cr.printData(cr.readTextInput())
+    cr.createOutput(cr.readTextInput())
 
 if __name__ == '__main__':
     main()
