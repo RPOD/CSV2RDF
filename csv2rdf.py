e5da3e35a6dd5577caa005f9b9172ed6ba91b7a1
Robert <mam09cqz@studserv.uni-leipzig.de>
2015-10-09 17:03:55 +0200

    added RDFLIB stuffs.

--- csv2rdf.py
+++ csv2rdf.py
@@ -6,8 +6,19 @@ from project import Project
 from person import Person
 from collections import namedtuple
 from urllib import parse
+from rdflib import Graph, URIRef, BNode, Literal, Namespace
+from rdflib.namespace import RDF, FOAF, DC, XSD, DOAP, RDFS
+import datetime
+
+DBO = Namespace("http://dbpedia.org/ontology/")
+CORDIS = Namespace("http://www.freme-project.eu/datasets/cordis/")
+CORG = Namespace(CORDIS.organizations + "/")
+CPRO = Namespace(CORDIS.projects + "/")
+CPEO = Namespace(CORDIS.people + "/")
+DBR = Namespace("http://dbpedia.org/resource/")
 
 class Csv2Rdf:
+
 
     def __init__(self, filename):
         self.filename = filename
@@ -285,6 +296,173 @@ class Csv2Rdf:
             print(i)
         of.close
 
+    def parseCordisProjectRDF(self, entry, graph, hostBene):
+        Project = namedtuple('Project', 'identifier, referenceID, name, title, homepage, startDate, endDate, status, programme, frameworkProgramme, topics, fundingScheme, budgetTotal, budgetFunding, coordinator, participants, subjects, objective')
+        project = []
+        project.append(entry[0])
+        project.append(entry[1])
+        project.append(entry[2])
+        project.append(entry[7])
+        project.append(entry[10])
+        project.append(entry[8])
+        project.append(entry[9])
+        if len(entry[3]) > 1:
+            project.append(self.transcribeStatus(entry[3]))
+        else:
+            project.append('')
+        project.append(entry[4])
+        project.append(entry[6])
+        project.append(entry[5])
+        project.append(entry[14])
+        project.append(entry[12])
+        project.append(entry[13])
+        project.append(entry[16])
+        tmp = []
+        for participant in entry[18].split(';'):
+            tmp.append(participant)
+        project.append(tmp)
+        tmp2 = []
+        for subject in entry[20].split(';'):
+            tmp2.append(subject)
+        project.append(tmp2)
+        project.append(entry[11])
+        """
+        Create triples
+        """
+        pro = Project(*project)
+        proRDF = CPRO[pro.identifier]
+        graph.add((proRDF, RDF.type, DBO.ResearchProject))
+        graph.add((proRDF, DBO.projectReferenceID, Literal(self.setLiterals(pro.referenceID))))
+        graph.add((proRDF, DOAP.name, Literal(self.setLiterals(pro.name))))
+        graph.add((proRDF, RDFS.label, Literal(self.setLiterals(pro.name))))
+        graph.add((proRDF, DC.title, Literal(self.setLiterals(pro.title))))
+        if len(pro.homepage) > 1:
+            graph.add((proRDF, DOAP.homepage, URIRef(pro.homepage)))
+        if len(pro.startDate) > 1:
+            sDate = datetime.date(int(pro.startDate.split('/')[2]), int(pro.startDate.split('/')[0]), int(pro.startDate.split('/')[1]))
+            eDate = datetime.date(int(pro.endDate.split('/')[2]), int(pro.endDate.split('/')[0]), int(pro.endDate.split('/')[1]))
+            graph.add((proRDF, DBO.projectStartDate, Literal(sDate)))
+            graph.add((proRDF, DBO.projectEndDate, Literal(eDate)))
+        if len(pro.status) > 1:
+            graph.add((proRDF, CORDIS.status, Literal(self.setLiterals(pro.status))))
+        graph.add((proRDF, CORDIS.programme, Literal(self.setLiterals(pro.programme))))
+        graph.add((proRDF, CORDIS.frameworkProgramme, Literal(self.setLiterals(pro.frameworkProgramme))))
+        graph.add((proRDF, CORDIS.projectTopics, Literal(self.setLiterals(pro.topics))))
+        if len(pro.fundingScheme) > 1:
+            graph.add((proRDF, CORDIS.projectFundingScheme, Literal(self.setLiterals(pro.fundingScheme))))
+        graph.add((proRDF, DBO.projectBudgetFunding, Literal(self.setLiterals(pro.budgetFunding.replace(',','.'))) + '^^<http://dbpedia.org/datatype/euro>'))
+        graph.add((proRDF, DBO.projectBudgetTotal, Literal(self.setLiterals(pro.budgetTotal.replace(',','.'))) + '^^<http://dbpedia.org/datatype/euro>'))
+        graph.add((proRDF, DBO.projectCoordinator, CORG[parse.quote_plus(pro.coordinator)]))
+        if len(pro.participants) > 1:
+            for participant in pro.participants:
+                graph.add((proRDF, DBO.projectParticipant, CORG[parse.quote_plus(participant)]))
+        for org in hostBene:
+            if pro.identifier == org[1]:
+                if org[0] == 'hostInstitution':
+                    graph.add((proRDF, CORDIS.hostInstitution, CORG[parse.quote_plus(org[2])]))
+                elif org[0] == 'beneficiary':
+                    graph.add((proRDF, CORDIS.beneficiary, CORG[parse.quote_plus(org[2])]))
+        if len(pro.subjects) > 1:
+            for subject in pro.subjects:
+                graph.add((proRDF, CORDIS.projectSubject, Literal(self.setLiterals(subject))))
+        graph.add((proRDF, DBO.projectObjective, Literal(self.setLiterals(pro.objective))))
+        #return self.createProjectOutput(Project(*project), hostBene)
+
+    def parseCordisOrganizationRDF(self, entry, graph):
+        Organization = namedtuple('Organization', 'identifier, referenceID, projectName, role, name, shortName, country, activityType, endOfParticipation, city, postalCode, street, homepage, contact')
+        org = []
+        org.append(entry[0])
+        org.append(entry[1])
+        org.append(entry[2])
+        org.append(entry[3])
+        org.append(entry[5])
+        org.append(entry[6])
+        if len(entry[10]) > 1:
+            org.append(self.alpha2Name(entry[10]))
+        else:
+            org.append('')
+        org.append(entry[7])
+        org.append(entry[8])
+        org.append('http://dbpedia.org/page/' + parse.quote(self.capitalizeAll(self.setLiterals(entry[12]))))
+        org.append(entry[13])
+        org.append( entry[11])
+        org.append((entry[14][:4] != 'http')*'http://' + entry[14])
+        org.append(parse.quote_plus(entry[17] + entry[18] + entry[6]))
+        organization = Organization(*org)
+        """
+        Create triples
+        """
+        orgRDF = CORG[parse.quote_plus(organization.name)]
+        graph.add((orgRDF, RDF.type, FOAF.Organization))
+        graph.add((orgRDF, CORDIS.organizationName, Literal(self.setLiterals(organization.name))))
+        graph.add((orgRDF, CORDIS.organizationShortName, Literal(self.setLiterals(organization.shortName))))
+        if len(organization.country) > 1:
+            graph.add((orgRDF, CORDIS.organizationCountry, DBR[organization.country]))
+        if len(organization.activityType) > 1:
+            graph.add((orgRDF, CORDIS.activityType, Literal(self.setLiterals(organization.activityType))))
+        if len(organization.endOfParticipation) > 1:
+            graph.add((orgRDF, CORDIS.endOfParticipation, Literal(self.setLiterals(organization.activityType))))
+        if len(organization.city) > 1:
+            graph.add((orgRDF, DBO.locationCity, URIRef(organization.city)))
+        if len(organization.street) > 1:
+            graph.add((orgRDF, DBO.address, Literal(self.setLiterals(organization.street))))
+        if len(organization.postalCode) > 1:
+            graph.add((orgRDF, DBO.postalCode, Literal(self.setLiterals(organization.postalCode))))
+        if len(organization.homepage) > 1:
+            graph.add((orgRDF, FOAF.homepage, URIRef(self.setLiterals(organization.homepage))))
+        if len(organization.contact) > 1:
+            graph.add((orgRDF, FOAF.person, CPEO[organization.contact]))
+        return organization
+
+    def parseCordisPersonRDF(self, entry, graph):
+        Person = namedtuple('Person', 'type, title, firstName, lastName, phone, fax, mail, shortOrgName')
+        person = []
+        person.append(entry[15])
+        person.append(entry[16])
+        person.append(entry[17])
+        person.append(entry[18])
+        person.append(entry[20])
+        person.append(entry[21])
+        person.append(entry[22])
+        person.append(entry[6])
+        per = Person(*person)
+        """
+        create Triples
+        """
+        perRDF = CPEO[parse.quote_plus(per.firstName + per.lastName + per.shortOrgName)]
+        graph.add((perRDF, RDF.type, FOAF.person))
+        if len(per.title) > 1:
+            graph.add((perRDF, FOAF.title, Literal(self.setLiterals(per.title))))
+        if len(per.firstName) > 1:
+            graph.add((perRDF, FOAF.firstName, Literal(self.setLiterals(per.firstName))))
+        if len(per.lastName) > 1:
+            graph.add((perRDF, FOAF.lastName, Literal(self.setLiterals(per.lastName))))
+        if len(per.phone) > 1:
+            graph.add((perRDF, FOAF.phone, Literal(self.setLiterals(per.phone))))
+        if len(per.fax) > 1:
+            graph.add((perRDF, CORDIS.faxNumber, Literal(self.setLiterals(per.fax))))
+        if len(per.mail) > 1:
+            graph.add((perRDF, FOAF.mbox, URIRef(per.mail)))
+        #return [self.createPersonOutput(output)]
+
+    def createCordisObjectsRDF(self, projectsData, organizationData):
+        usedOrgs = []
+        hostBene = []
+        graph = Graph()
+        print('start')
+        for i, organization in enumerate(organizationData[1:]):
+            if (not organization[5] in usedOrgs):
+                org = self.parseCordisOrganizationRDF(organization, graph)
+                usedOrgs.append(org.name)
+                self.parseCordisPersonRDF(organization, graph)
+                if org.role == 'hostInstitution' or org.role == 'beneficiary':
+                    hostBene.append([org.role, org.identifier, org.name])
+            print(i)
+        for i, project in enumerate(projectsData[1:]):
+            self.parseCordisProjectRDF(project, graph, hostBene)
+            print(i)
+        graph.serialize('cordis_full_RDF.ttl', format='turtle')
+
     def createProjectOutput(self, project, hostBene):
         output = ('cordis:projects/' + project.identifier + ' a dbo:ResearchProject;\n\t' +
                   'dpro:projectReferenceID\t"' + self.setLiterals(project.referenceID) + '";\n\t' +
@@ -422,6 +600,7 @@ class Csv2Rdf:
 
 
 def main():
+
     file = input('Name of .csv file:')
     cr = Csv2Rdf(file)
     if 'cordis' in file:
@@ -434,9 +613,26 @@ def main():
             organizationsData = cr.readMultilineInput('ᛥ')
             cr.filename = 'cordis_projects'
             projectsData = cr.readMultilineInput('ᛥ')
-        cr.createCordisObjects(projectsData, organizationsData)
+        cr.createCordisObjectsRDF(projectsData, organizationsData)
     else:
         cr.createOutput(cr.readTextInput(';'))
-
+    """
+    g = Graph()
+    bob = URIRef("http://example.org/people/Bob")
+    linda = URIRef("http://example.org/people/Linda")
+    DBO = Namespace("http://dbpedia.org/ontology/")
+    CORDIS = Namespace("http://www.freme-project.eu/datasets/cordis/")
+    test = "FUG"
+    CORG = Namespace(CORDIS.organizations + "/")
+    print(CORDIS[test])
+    print(CORG.orgname)
+    g.add( (CORG.Orgname, RDF.type, FOAF.Organization) )
+    g.add( (CORG.Orgname, RDF.type, DBO.Organisation) )
+    g.add( (bob, FOAF.name, Literal('Bob')) )
+    g.add( (bob, FOAF.knows, linda) )
+    g.add( (linda, RDF.type, FOAF.Person) )
+    g.add( (linda, FOAF.name, Literal('Linda') ) )
+    print (g.serialize(format='turtle'))
+    """
 if __name__ == '__main__':
     main()
